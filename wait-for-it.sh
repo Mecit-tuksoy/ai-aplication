3c0812a9a04bb5a2e37a80c611bb89cd31603d57
Mecit-tuksoy <mecit.tuksoy@gmail.com>
2024-05-12 20:54:37 +0300

    first

---
+++ wait-for-it.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# wait-for-it.sh
+
+host="$1"
+port="$2"
+shift 2
+cmd="$@"
+
+echo "Waiting for $host:$port..."
+
+while ! nc -z "$host" "$port"; do
+  sleep 0.1
+done
+
+echo "Service is available!"
+
+exec $cmd
